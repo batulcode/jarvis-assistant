f2a06e833eb455d2c38b0bc719e402cd5f2058f7
batulcode <batulsabunwala21@gmail.com>
2026-06-10 20:57:15 +0530

    Add speech recognition functionality

--- script.js
+++ script.js
@@ -31,3 +31,26 @@ function testVoice() {
     speech.lang = "en-US";
     speechSynthesis.speak(speech);
 }
+function startListening() {
+
+const recognition =
+new(window.SpeechRecognition ||
+window.webkitSpeechRecognition)();
+
+recognition.lang = "en-US";
+
+recognition.start();
+
+recognition.onresult = function(event) {
+
+let command =
+event.results[0][0].transcript;
+
+document.getElementById("userInput").value =
+command;
+
+talk();
+
+};
+
+}
